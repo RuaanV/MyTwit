c39bd00e7c5817ed5ce621387e1817dff01694cf
Ruaan Venter <ruaan_venter@msn.com>
2011-03-26 13:08:39 +0000

    Configured Excluded

--- MyTwit/MyTwit/MainPage.xaml.cs
+++ MyTwit/MyTwit/MainPage.xaml.cs
@@ -10,15 +10,45 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Silverlight.Testing;
 
 namespace MyTwit
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
-        public MainPage()
-        {
-            InitializeComponent();
+
+        private const string ApplicationTitleName = "My Twitter";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+          public MainPage()
+          {
+              InitializeComponent();
+            this.ApplicationTitle.Text = ApplicationTitleName;
+
+            CheckForUnitTestRunner();
         }
+
+        /// <summary>
+        /// Checks for unit test runner.
+        /// </summary>
+        private void CheckForUnitTestRunner()
+        {
+            const bool runUnitTests = true;
+
+            if (runUnitTests)
+            {
+                Content = UnitTestSystem.CreateTestPage();
+                var imtp = Content as IMobileTestPage;
+
+                if (imtp != null)
+                {
+                    BackKeyPress += (x, xe) => xe.Cancel =
+                            imtp.NavigateBack();
+                }
+
+            }
+          }
+      }
     }
-}
